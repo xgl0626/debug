d206136e6029a965c4d643cc258ba34cd27ad8e2
RayleighZ <3226139433@qq.com>
2021-03-28 03:21:26 +0800

    [feature]ui页面绘制

---
+++ app/src/main/java/com/example/d2doctor/ui/activity/FeatureDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.d2doctor.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.d2doctor.R
+
+class FeatureDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feature_detail)
+    }
+}
